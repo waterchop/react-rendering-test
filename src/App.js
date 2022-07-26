c8db0c79d463287e84dfd543ffc07ab3748466c7
ImperialWatanabe <59148799+waterchop@users.noreply.github.com>
2022-07-26 23:49:39 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { ChildArea } from "./ChildArea/ChildArea";
+import "./styles.css";
+
+export default function App() {
+  console.log("app");
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  const onClickOpen = () => {
+    setOpen(!open);
+  };
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
+    </div>
+  );
+}
---
+++ src/ChildArea/ChildArea.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = (props) => {
+  const { open } = props;
+  console.log("ChildAreaがレンダリングされた！");
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log("...");
+  });
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
